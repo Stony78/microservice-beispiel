badca5ba247e5d49797c7983c2053c716762c0dd
Herwig Steininger <herwig.steininger@iteratec.at>
2019-07-17 14:21:54 +0200

    init

--- src/main/java/at/iteratec/calculator/beispiel/Application.java
+++ src/main/java/at/iteratec/calculator/beispiel/Application.java
@@ -2,8 +2,12 @@ package at.iteratec.calculator.beispiel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableSwagger2
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
