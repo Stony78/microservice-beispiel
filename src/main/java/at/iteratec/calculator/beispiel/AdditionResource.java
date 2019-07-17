29076e3757e0ab75eb65624dbf21b3a58108e959
Herwig Steininger <herwig.steininger@iteratec.at>
2019-07-17 16:50:27 +0200

    init

--- src/main/java/at/iteratec/calculator/beispiel/AdditionResource.java
+++ src/main/java/at/iteratec/calculator/beispiel/AdditionResource.java
@@ -1,19 +1,27 @@
 package at.iteratec.calculator.beispiel;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
 public class AdditionResource {
     @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
     @Autowired
     private LoggingService loggingService;
 
+    @Value("${radix}")
+    private int radix;
+
     @GetMapping("/api/add")
-    public String add(@RequestParam int o1, @RequestParam int o2) {
-        String result = String.valueOf(o1 + o2);
+    public String add(@RequestParam String o1, @RequestParam String o2) {
+        int v1 = Integer.parseInt(o1, radix);
+        int v2 = Integer.parseInt(o2, radix);
+        String result = Integer.toString(v1 + v2, radix);
         loggingService.log("John Doe", o1 + " + " + o2 + " = " + result);
 
         return result;
